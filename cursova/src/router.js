8f5cab3d10d0ab353307c7d6062a44a6c1a06509
IvanOndok75 <kotikpusistik417@gmail.com>
2026-07-05 13:11:38 +0300

    Update router.js

--- cursova/src/router.js
+++ cursova/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import IntroPage from '@/components/IntroPage.vue';
 import PageF1 from '@/pages/PageF1.vue';
 import PageF2 from '@/pages/PageF2.vue';
@@ -52,7 +52,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
